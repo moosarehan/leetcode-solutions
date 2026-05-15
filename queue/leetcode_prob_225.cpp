e271300b8624c1b421fa83bb185853a75aa4bcb3
moosarehan <moosarehan4@gmail.com>
2026-05-15 23:29:39 +0500

    Add solution for Problem 225: Implement Stack using Queues. Implement a LIFO stack using two queues.

---
+++ queue/leetcode_prob_225.cpp
@@ -0,0 +1,59 @@
+class MyStack {
+public:
+queue<int> q1;
+queue<int>q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        if(empty())
+        {
+            return -1;
+        }
+       while(q1.size()>1)
+       {
+           q2.push(q1.front());
+           q1.pop();
+       }
+       int x=q1.front();
+       q1.pop();
+       while(!q2.empty())
+       {
+           q1.push(q2.front());
+           q2.pop();
+       }
+       return x;
+
+    }
+    
+    int top() {
+      if(empty())
+      {
+        return -1;
+      }
+       return q1.back();
+    }
+    
+    bool empty() {
+        if(q1.empty())
+        {
+            return true;
+        }
+        else
+        return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
