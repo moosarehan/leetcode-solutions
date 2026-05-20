0c90ecf65be1c9b467c6d7bdc818ada308162432
moosarehan <moosarehan4@gmail.com>
2026-05-20 23:29:57 +0500

    LeetCode 2073: Time Needed to Buy Tickets
    Calculate the total time required for the person at a specific index k in a queue to finish buying all their desired tickets.

---
+++ queue/leetcode_prob_2073.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        queue<int>q;
+        for(int i=0;i<tickets.size();i++)
+        {
+            q.push(i);
+        }
+        int time=0;
+        while(tickets[k]!=0)
+        {
+            tickets[q.front()]--;
+            if(tickets[q.front()]==0)
+            {
+                q.pop();
+            }
+            else
+            {
+                 q.push(q.front());
+                 q.pop();
+            }
+            time+=1;
+        }
+        return time;
+    }
+};
